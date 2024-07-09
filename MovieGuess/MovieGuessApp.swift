b2105da77ec813c2ca43a8f874645655979ba2c8
Christopher Cordero <cjcorder@usc.edu>
2024-07-09 18:33:27 -0400

    initial commit

---
+++ MovieGuess/MovieGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGuessApp.swift
+//  MovieGuess
+//
+//  Created by Christopher Cordero on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
